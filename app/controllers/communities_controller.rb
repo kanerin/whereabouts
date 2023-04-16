110e014bc062450a63cae5c37865d26ef25adf92
kanerin <kanerin1004@gmail.com>
2023-04-16 20:04:06 +0900

    create community new

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,2 +1,9 @@
 class CommunitiesController < ApplicationController
+  def index
+  end
+  
+  def new
+    @Community = Community.new
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
 
   def index
     @user = current_user
-    flash[:notice] = "テストメッセージ"
   end
   
 end
